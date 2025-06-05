f50cc7f50f0c9a4551a2a13ede29f2f0ffaf52f1
Lucie B <bizova.lucie@gmail.com>
2025-06-05 14:02:03 +0200

    úvodní zprávy na obrazovce po spuštění programu.

--- Vyhledavac_vuni/Program.cs
+++ Vyhledavac_vuni/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Vyhledavac_vuni;
+﻿using System.Net;
+
+namespace Vyhledavac_vuni;
 
 class Program
 {
     static void Main(string[] args)
     {
-       // Console.WriteLine("Hello, World!");
+        Console.WriteLine("Vítejte v aplikaci pro vyhledávání vůní a správu seznamu vůní.");
+
+        //budoucí while
+        Console.WriteLine();
+        Instructions0();
+        string input = Console.ReadLine() ?? string.Empty;
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            System.Console.WriteLine("neplatný příkaz. Zkuste to znovu. ");
+            //continue;
+        }
+    }
+    private static void Instructions0()
+    {
+        Console.WriteLine($"Co chcete dělat? (zadejte číslo): \n 1. VYHLEDÁVAT, \n 2. DOPLNIT VŮNI, \n 3. ODSTRANIT VŮNI \n 4. ULOŽIT ZMĚNY \n 5. UKONČIT PROGRAM");
     }
 }
